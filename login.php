dc2025ecf3eab1d27549b825833cadae6e955588
DilshanaRanawake <dilshanaranawake@gmail.com>
2025-01-30 23:54:52 +0530

    adding login function

--- login.php
+++ login.php
@@ -19,7 +19,11 @@
             <div class="alert alert-success" role="alert">
                 <?php echo stripslashes($_GET['success']);?>
             </div>
-        <?php } ?>
+        <?php } 
+            // $pass = "123";
+            // $pass = password_hash($pass, PASSWORD_DEFAULT);
+            // echo $pass;
+        ?>
         
         
         <div class="mb-3">
