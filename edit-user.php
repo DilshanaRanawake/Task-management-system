bbc80cd7acb8f511f6a3f82df0c58f1dbbec7d7f
DilshanaRanawake <dilshanaranawake@gmail.com>
2025-02-03 22:39:28 +0530

    Adding delete function

--- edit-user.php
+++ edit-user.php
@@ -24,7 +24,7 @@ if (isset($_SESSION['role'])&& isset($_SESSION['id'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Add Users</title>
+    <title>Edit Users</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="css/style.css">
 </head>
